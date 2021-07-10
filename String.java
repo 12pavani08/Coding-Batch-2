e161f7c89306cc827746235435d4964dca9580ed
12pavani08 <86842838+12pavani08@users.noreply.github.com>
2021-07-10 23:58:51 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,13 @@
+class RevString
+{
+public static void main (String args [])
+{
+String s="pavani";
+String rev=" ";
+for(int i=s.length()-1;i>=0;i--)
+{
+rev=rev+s.charAt(i);
+}
+System.out.prinrln(rev);
+}
+}
